79709ada0b867ea4d68406ad0f88aeceba4f8d30
EArriagada <arriagada.ed@gmail.com>
2024-07-29 15:20:33 -0400

    primer comit

---
+++ src/main/java/com/infodev/appfitness/AppfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.infodev.appfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfitnessApplication.class, args);
+	}
+
+}
